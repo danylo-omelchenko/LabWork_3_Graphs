aaefa11707e16086d62eaf814396753d16d0f27e
Даниил <dpixelstudio@gmail.com>
2014-06-18 15:46:42 +0300

    324

--- GraphView/Form1.cs
+++ GraphView/Form1.cs
@@ -6,7 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using Graph;
+using GraphImplementation;
 
 namespace GraphView
 {
@@ -18,7 +18,7 @@ namespace GraphView
             
         }
 
-        Graph.Graph g;
+        Graph g;
 
     }
 }
